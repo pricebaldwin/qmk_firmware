5ff1640b65189c2cab2e6e24c514cb0f72c6b0ca
Price Baldwin <price@pricebaldwin.com>
2024-06-01 17:30:59 -0400

    Add my initial firmware for the Planck

---
+++ keyboards/planck/rev7/keymaps/pricebaldwin/config.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+#endif
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 8
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 40
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 5
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 30
+
+#define FIRMWARE_VERSION u8"brWJx/Gar6w"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_16BIT
+#define COMBO_COUNT 3
+
